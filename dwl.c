52dbf2ca1507269447c4716502e313788b66135b
Benjamin Chausse <benjamin@chausse.xyz>
2024-07-22 23:42:31 -0400

    Implement togglekblayoutandoptions patch
    https://codeberg.org/dwl/dwl-patches/src/branch/main/patches/togglekblayoutandoptions

--- dwl.c
+++ dwl.c
@@ -335,6 +335,7 @@ static void setsel(struct wl_listener *listener, void *data);
 static void setup(void);
 static void spawn(const Arg *arg);
 static void startdrag(struct wl_listener *listener, void *data);
+static void incxkbrules(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
@@ -404,6 +405,7 @@ static struct wl_listener lock_listener = {.notify = locksession};
 
 static struct wlr_seat *seat;
 static KeyboardGroup *kb_group;
+static unsigned int kblayout = 0;
 static unsigned int cursor_mode;
 static Client *grabc;
 static int grabcx, grabcy; /* client-relative */
@@ -890,7 +892,7 @@ createkeyboardgroup(void)
 
 	/* Prepare an XKB keymap and assign it to the keyboard group. */
 	context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
-	if (!(keymap = xkb_keymap_new_from_names(context, &xkb_rules,
+	if (!(keymap = xkb_keymap_new_from_names(context, &xkb_rules[kblayout],
 				XKB_KEYMAP_COMPILE_NO_FLAGS)))
 		die("failed to compile keymap");
 
@@ -2664,6 +2666,25 @@ startdrag(struct wl_listener *listener, void *data)
 
 	drag->icon->data = &wlr_scene_drag_icon_create(drag_icon, drag->icon)->node;
 	LISTEN_STATIC(&drag->icon->events.destroy, destroydragicon);
+}
+
+void
+incxkbrules(const Arg *arg)
+{
+	KeyboardGroup *group;
+	struct wlr_keyboard_group *wlr_group;
+	const struct xkb_rule_names newrule = xkb_rules[(kblayout + 1) % LENGTH(xkb_rules)];
+
+	wl_list_for_each(group, &kb_group->link, link) {
+		wl_list_for_each(wlr_group, &group->wlr_group->keys, keys) {
+			struct xkb_context *context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+			struct xkb_keymap *keymap = xkb_map_new_from_names(context, &newrule,
+					XKB_KEYMAP_COMPILE_NO_FLAGS);
+			wlr_keyboard_set_keymap(&wlr_group->keyboard, keymap);
+			xkb_keymap_unref(keymap);
+			xkb_context_unref(context);
+		}
+	}
 }
 
 void
